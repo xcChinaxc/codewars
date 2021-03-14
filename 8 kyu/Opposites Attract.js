5c202ed2ed158a43ae004635fbeab9cd91231fde
asweetscorpio <67491204+asweetscorpio@users.noreply.github.com>
2021-03-14 14:21:47 -0400

    completed codewars

---
+++ 8 kyu/Opposites Attract.js
@@ -0,0 +1,5 @@
+/* Opposites Attract */
+
+function lovefunc(flower1, flower2){
+  return ((flower1 % 2 === 0 && flower2 % 2 === 1) || (flower2 % 2 === 0 && flower1 % 2 === 1) ? true : false )
+}
---
+++ 8 kyu/You Can't Code Under Pressure #1.js
@@ -0,0 +1,6 @@
+/* You Can't Code Under Pressure #1 */
+
+function doubleInteger(i) {
+  // i will be an integer. Double it and return it.
+  return i + i;
+}
