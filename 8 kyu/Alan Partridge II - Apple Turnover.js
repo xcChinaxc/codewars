2289b8d22834c6d3c9282fbb29537ad5a4d93b42
xcChinaxc <67491204+xcChinaxc@users.noreply.github.com>
2021-03-24 17:24:26 -0400

    if x squared is greater than 1000 return quote

---
+++ 8 kyu/Alan Partridge II - Apple Turnover.js
@@ -0,0 +1,6 @@
+/* Alan Partridge II - Apple Turnover */
+
+function apple(x){
+    if(Math.pow(Number(x),2) > 1000){return `It's hotter than the sun!!`}
+    else{return `Help yourself to a honeycomb Yorkie for the glovebox.`}
+  }
