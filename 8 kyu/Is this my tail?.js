d51dc56af79637f1441070a4238e6c3513ce174a
asweetscorpio <67491204+asweetscorpio@users.noreply.github.com>
2021-03-26 08:42:03 -0400

    return true if tail is correct return false if not

---
+++ 8 kyu/Is this my tail?.js
@@ -0,0 +1,12 @@
+/* Is this my tail? */
+
+function correctTail(body, tail){
+  sub = body.substr(body.length-(tail.length))
+  
+  if(sub === tail){
+    return true
+  }
+  else { 
+    return false
+  }
+}
