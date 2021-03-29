489c1b2124108c96e5cfa502801fbb34413fea96
asweetscorpio <67491204+asweetscorpio@users.noreply.github.com>
2021-03-29 10:13:51 -0400

    ball objects should return regular or super

---
+++ 8 kyu/Regular Ball Super Ball.js
@@ -0,0 +1,10 @@
+/* Regular Ball Super Ball */
+
+/* Create a class Ball. Ball objects should accept one argument for "ball type" when instantiated. If no arguments are given, ball objects should instantiate with a "ball type" of "regular." */
+
+
+class Ball {
+  constructor(ballType){
+    this.ballType = ballType || "regular"
+  }
+}
