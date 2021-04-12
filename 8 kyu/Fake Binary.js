2ba8e8583dad549f3f881865b4064fa4dd563dc6
xcChinaxc <67491204+xcChinaxc@users.noreply.github.com>
2021-04-12 16:13:03 -0400

    replace digits 4 and below with 0, others with 1

---
+++ 8 kyu/Fake Binary.js
@@ -0,0 +1,7 @@
+/* Fake Binary */
+/* Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+ */
+
+function fakeBin(x){
+  return [...x].map( y => y < 5 ? 0 : 1).join('')
+}
