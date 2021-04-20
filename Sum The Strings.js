b5d02a31fc129da3dbce74cb49678979d3d54149
asweetscorpio <67491204+asweetscorpio@users.noreply.github.com>
2021-04-19 23:00:32 -0400

    take 2 strings, add them, return sum as string

---
+++ Sum The Strings.js
@@ -0,0 +1,12 @@
+/* Sum The Strings */
+
+/* Create a function that takes 2 positive integers in form of a string as an input, and outputs the sum (also as a string): */
+
+// P - two nums as strings
+// R - sum of two nums as strings
+// E - sumStr("4", "5")
+// P - make strings numbers, add numbers, make string again
+
+function sumStr(a,b) {
+    return String(Number(a) + Number(b))
+  }
