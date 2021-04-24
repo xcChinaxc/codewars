1e082e9b48d7a3833ef3f94f7c8369e1c7b86a1f
asweetscorpio <67491204+asweetscorpio@users.noreply.github.com>
2021-04-23 20:41:16 -0400

    transform a string into a number

---
+++ 8 kyu/Convert a String to a Number!.js
@@ -0,0 +1,13 @@
+/* Convert a String to a Number! */
+
+/* We need a function that can transform a string into a number. What ways of achieving this do you know? */
+
+// P - string
+// R - num
+// E - stringToNumber("-7"),  -7
+// P - Number method
+
+var stringToNumber = function(str){
+    return Number(str)
+}
+  
