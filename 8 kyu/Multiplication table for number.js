95f200299b2047009c1004b43a84d52283ebaebf
xcChinaxc <67491204+xcChinaxc@users.noreply.github.com>
2021-05-19 12:26:37 -0400

    multiplication table for number

---
+++ 8 kyu/Multiplication table for number.js
@@ -0,0 +1,7 @@
+/* Multiplication table for number */
+
+/* Your goal is to return multiplication table for number that is always an integer from 1 to 10. */
+
+function multiTable(number) {
+  return `1 * ${number} = ${1 * number}\n2 * ${number} = ${2 * number}\n3 * ${number} = ${3 * number}\n4 * ${number} = ${4 * number}\n5 * ${number} = ${5 * number}\n6 * ${number} = ${6 * number}\n7 * ${number} = ${7 * number}\n8 * ${number} = ${8 * number}\n9 * ${number} = ${9 * number}\n10 * ${number} = ${10 * number}`
+}
