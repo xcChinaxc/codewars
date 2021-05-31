06e58fdbbe35d4c2627d42bf5a3a8963675bb3b1
asweetscorpio <67491204+asweetscorpio@users.noreply.github.com>
2021-05-31 13:16:21 -0400

    return i to the power of a given non-negative num

---
+++ 7 kyu/Powers of i.js
@@ -0,0 +1,9 @@
+/* Powers of i */
+
+/* i is the imaginary unit, it is defined by i² = -1, therefore it is a solution to x²+1=0.
+Your task is to write a function pofi that returns i to the power of a given non-negative integer in its simplest form as a string (answer may contain i).  */
+
+function pofi(n) {
+  let key = { 0: '1', 1: 'i', 2: '-1', 3: '-i' };
+  return key[n % 4];
+}
