ca81916607630a038a5eb8717289ae55a7d41fb0
asweetscorpio <67491204+asweetscorpio@users.noreply.github.com>
2021-06-07 21:49:14 -0400

    remove every second element from the array

---
+++ 8 kyu/Removing Elements.js
@@ -0,0 +1,8 @@
+/* Removing Elements */
+
+/* Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+ */
+
+function removeEveryOther(arr){
+    return arr.filter((a,i) => i%2===0)
+}
