92d6acf3a717d52f7b9356259a396ea535ed08bd
asweetscorpio <67491204+asweetscorpio@users.noreply.github.com>
2021-06-09 20:18:03 -0400

    repeat the string n number of times

---
+++ 7 kyu/Thinkful - String Drills: Repeater.js
@@ -0,0 +1,7 @@
+/* Thinkful - String Drills: Repeater */
+
+/* Write a function named repeater() that takes two arguments (a string and a number), and returns a new string where the input string is repeated that many times. */
+
+function repeater(string, n){
+  return string.repeat(n)
+}
