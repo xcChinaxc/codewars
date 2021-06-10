5e8a4c17bfb083c2e4809010b95780aee4fd384e
asweetscorpio <67491204+asweetscorpio@users.noreply.github.com>
2021-06-09 20:16:02 -0400

    add two nba players points-per-game

---
+++ 7 kyu/All Star Code Challenge #1.js
@@ -0,0 +1,7 @@
+/* All Star Code Challenge #1 */
+
+/* Write a function, called sumPPG, that takes two NBA player objects/struct/Hash/Dict/Class and sums their PPG */
+
+function sumPPG(playerOne, playerTwo){
+    return playerOne.ppg + playerTwo.ppg
+}
