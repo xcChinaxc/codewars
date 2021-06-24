8284d65ef1e35d1c4a8c68a1ca597786ce12c1a4
xcChinaxc <67491204+xcChinaxc@users.noreply.github.com>
2021-06-24 16:33:37 -0400

    return last name & first name. swap positions.

--- 8 kyu/Name Shuffler.js
+++ 8 kyu/Name Shuffler.js
@@ -3,6 +3,12 @@
 Write a function that returns a string in which firstname is swapped with last name.   
 */
 
+// Attempt Two
+function nameShuffler(str){
+    return str.split(" ").reverse().join(" ")
+}
+
+// Attempt One
 function nameShuffler(str){
   let x = str.split(' ')
   return `${x[1]} ${x[0]}`
