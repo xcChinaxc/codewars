044156c1b58a37d8b736554bb1cb2745272e955d
xcChinaxc <67491204+xcChinaxc@users.noreply.github.com>
2021-06-29 22:02:18 -0400

    given an input 0-9, return input in words

---
+++ 8 kyu/Switch it Up!.js
@@ -0,0 +1,9 @@
+/* Switch it Up!
+
+When provided with a number between 0-9, return it in words.
+*/
+
+function switchItUp(number){
+  let nums = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine']
+  return nums[number]
+}
