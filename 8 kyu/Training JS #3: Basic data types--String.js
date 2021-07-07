113c9f9e9be58bfb3b96cc25b3e6e3c0bb8440fb
asweetscorpio <67491204+asweetscorpio@users.noreply.github.com>
2021-07-07 19:09:47 -0400

    given global variables, return name of function

---
+++ 8 kyu/Training JS #3: Basic data types--String.js
@@ -0,0 +1,24 @@
+/* Training JS #3: Basic data types--String
+
+I've create three function, and defined some global variables, please select some variables that can make up the name of the function, and return them(Please note the uppercase and lowercase letters are different).
+*/
+
+var a1="A",a2="a",b1="B",b2="b",c1="C",c2="c",d1="D",d2="d",e1="E",e2="e",n1="N",n2="n"
+function Dad(){
+  return d1+a2+d2;
+}
+function Bee(){
+  return b1+e2+e2;
+}
+function banana(){
+  return b2+a2+n2+a2+n2+a2;
+}
+function answer1(){
+  return "no";
+}
+function answer2(){
+  return "no";
+}
+function answer3(){
+  return "yes";
+}
