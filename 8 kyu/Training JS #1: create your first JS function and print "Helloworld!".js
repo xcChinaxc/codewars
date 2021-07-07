ece107c8e083136fbf576cf21500df21ffca7255
asweetscorpio <67491204+asweetscorpio@users.noreply.github.com>
2021-07-07 18:55:06 -0400

    print hello world to the console

---
+++ 8 kyu/Training JS #1: create your first JS function and print "Helloworld!".js
@@ -0,0 +1,12 @@
+/* Training JS #1: create your first JS function and print "Helloworld!"
+
+Please refer to two example above and write your first JS function.
+ - mission 1: use keyword function to define your function, function name should be helloWorld(don't forget the () and {})
+ - mission 2: use keyword var to define a variable str, value of str should be a string: "Hello World!"(don't forget the =).
+ - mission 3: type the console.log() in the next line(don't forget to put the str in the brackets).
+*/
+
+function helloWorld(){
+    var str = 'Hello World!'
+    console.log(str)
+}
