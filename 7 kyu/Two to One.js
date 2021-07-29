23f442ae5f2e6f2108cb398ebc16c9c97aa2ecce
China Redman <67491204+xcChinaxc@users.noreply.github.com>
2021-07-29 15:06:13 -0400

    combine strings & return sorted distinct letters

---
+++ 7 kyu/Two to One.js
@@ -0,0 +1,9 @@
+/* Two to One
+
+Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+*/
+
+function longest(s1, s2) {
+  let word = [...new Set(s1.concat(s2))]
+  return word.sort().join('')
+}
