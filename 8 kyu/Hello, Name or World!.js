889e4a55edbacf604e966ee4064f11e992869768
China Redman <67491204+xcChinaxc@users.noreply.github.com>
2021-07-30 22:46:55 -0400

    return given name or default string

---
+++ 8 kyu/Hello, Name or World!.js
@@ -0,0 +1,10 @@
+/* Hello, Name or World!
+
+Define a method hello that returns "Hello, Name!" to a given name, or says Hello, World! if name is not given (or passed as an empty String).
+
+Assuming that name is a String and it checks for user typos to return a name with a first capital letter (Xxxx).
+*/
+
+function hello(name) {
+  return 'Hello, ' + `${name ? name[0].toUpperCase() + name.toLowerCase().slice(1) + '!' : 'World!'}`;
+}
