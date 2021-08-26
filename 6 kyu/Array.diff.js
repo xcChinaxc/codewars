bb3a46b7b8e769ecc97f917cea681ecc098cf0ee
China Redman <67491204+xcChinaxc@users.noreply.github.com>
2021-08-26 16:45:39 -0400

    return values from list 1 that are NOT in list 2

---
+++ 6 kyu/Array.diff.js
@@ -0,0 +1,26 @@
+/* Array.diff
+
+Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+
+It should remove all values from list a, which are present in list b keeping their order.
+- arrayDiff([1,2],[1]) == [2]
+
+If a value is present in b, all of its occurrences must be removed from the other:
+- arrayDiff([1,2,2,2,3],[2]) == [1,3]
+*/
+
+// Method Work
+function arrayDiff(a, b) {
+  return a.filter( num => !b.includes(num))
+}
+
+// Brute Force
+function arrayDiff(a, b) {
+  let arr = []
+  
+  a.map( num => {
+    if(!b.includes(num)) arr.push(num)
+  })
+  
+  return arr
+}
